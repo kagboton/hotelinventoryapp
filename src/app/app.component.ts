2ff23692b48cd55fe0ee626ad1445ad12238cdcd
Kilian Agboton <agbotonstefane@gmail.com>
2024-06-04 13:16:50 -0400

    viewContainerRef to load component dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
@@ -6,14 +6,28 @@ import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RoomsComponent, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [
+    RouterOutlet,
+    RoomsComponent,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+  ],
   templateUrl: './app.component.html',
   //template: `<h1>Hi</h1>`,
   styleUrl: './app.component.css',
   //styles: [`h1 { font-family: Lato; color: red; }`]
 })
-export class AppComponent {
+export class AppComponent{
   title = 'hotelinventoryapp';
 
   role = 'Admin';
+
+  @ViewChild('rooms', { read: ViewContainerRef })
+  viewContainerRef!: ViewContainerRef; // helps dynamically load a component
+
+  ngAfterViewInit() { 
+    const componentRef = this.viewContainerRef.createComponent(RoomsComponent);
+    componentRef.instance.numberOfRooms = 57; // change component property
+  }
 }
